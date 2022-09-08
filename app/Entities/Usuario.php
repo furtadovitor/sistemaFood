8ad2e1fa6254c049cbdfce64839d34d4573012aa
Vitor Hugo <vitor.furtadopereira@gmail.com>
2022-09-08 11:07:27 -0300

    TEste

---
+++ app/Entities/Usuario.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Entities;
+
+use CodeIgniter\Entity\Entity;
+
+class Usuario extends Entity
+{
+    protected $dates   = [
+        'criado_em',
+        'atualizado_em',
+        'deletado_em',
+    ];
+
+    public function verificaPassword(string $password){
+
+        return password_verify($password, $this->password_hash);
+    }
+}
