4be7533dbe359665b6cf243a328844e2b2403614
furtadovitor <vitor.furtadopereira@gmail.com>
2022-10-11 15:55:46 -0300

    ajutes

--- app/Controllers/Carrinho.php
+++ app/Controllers/Carrinho.php
@@ -8,10 +8,20 @@ class Carrinho extends BaseController
 {
 
     private $validacao;
+    private $produtoEspecificacaoModel;
+    private $extraModel;
+    private $produtoModel;
+
 
     public function __construct(){
 
         $this->validacao = service('validation');
+
+        $this->produtoEspecificacaoModel = new \App\Models\ProdutoEspecificacaoModel();
+        $this->extraModel = new \App\Models\ExtraModel();
+        $this->produtoModel = new \App\Models\ProdutoModel();
+
+
     }
     public function index()
     {
@@ -31,6 +41,7 @@ class Carrinho extends BaseController
                 'produto.preco' => ['label' => 'Valor do produto', 'rules' => 'required|greater_than[0]'],
                 'produto.quantidade' => ['label' => 'Quantidade', 'rules' => 'required|greater_than[0]'],
 
+
             ]);
 
             //Se o formulário não for validado, eu mostro os erros de validação
@@ -39,6 +50,44 @@ class Carrinho extends BaseController
                 return redirect()->back()->with('errors_model', $this->validacao->getErrors())->with('atencao', 'Por favor, verifique os erros abaixo e tente novamente.')->withInput();
 
             }
+
+            /* validando a existência da especificacao_id*/
+
+            $especificacaoProduto = $this->produtoEspecificacaoModel->where('id', $produtoPost['especificacao_id'])->first();
+
+            //retornando para a index se o especificacaoProduto for nulo
+            if($especificacaoProduto == null){
+
+                return redirect()->back()->with('fraude', 'Não conseguimos processar a sua solicitação. Favor entrar em contato com a equipe informando o código de erro <strong>ERRO - 001</strong>'); //alteração no form
+
+            }
+
+            //caso, o extra id venha no posto, valido o extra
+
+            if($produtoPost['extra_id'] && $produtoPost['extra_id'] !=  "" ){
+
+                $extra = $this->extraModel->where('id', $produtoPost['extra_id'])->first();
+            
+                if($extra == null){
+
+                    return redirect()->back()->with('fraude', 'Não conseguimos processar a sua solicitação. Favor entrar em contato com a equipe informando o código de erro <strong>ERRO - 002</strong>'); //alteração no form chave extra_id
+    
+            
+            }
+        }
+
+        //validandos a existência do produtos 
+
+        $produto = $this->produtoModel->where('slug', $produtoPost['slug'])->first();
+
+
+        //validando a existencia do produto e se o msm está ativo
+        if($produto == null || $produto->ative == false){
+
+            return redirect()->back()->with('fraude', 'Não conseguimos processar a sua solicitação. Favor entrar em contato com a equipe informando o código de erro <strong>ERRO - 003</strong>'); //alteração no form na chave produto_slug
+
+        }
+
 
         }else{
 
