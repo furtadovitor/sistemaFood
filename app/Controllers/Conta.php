92d4e35d12297a0857dd901cfc9fc672030c3aed
VitorHugo <101369856+Pitileco@users.noreply.github.com>
2022-10-31 22:26:02 -0300

    autenticacao

--- app/Controllers/Conta.php
+++ app/Controllers/Conta.php
@@ -33,4 +33,66 @@ class Conta extends BaseController
 
 
     }
+
+    public function editar() {
+
+        if(!session()->has('pode_editar_ate')){
+
+            return redirect()->to(site_url('conta/autenticar'));
+
+        }
+
+        if(session()->get('pode_editar_ate') < time())
+
+        $data = [
+
+            'titulo' => 'Editar meus dados',
+            'usuario' => $this->usuario,
+        ];
+
+        return view('Conta/editar', $data);
+
+    }
+
+    public function autenticar()
+    {
+
+        $data = [
+
+            'titulo' => 'Por favor, se autentique novamente.',
+            'usuario' => $this->usuario,
+        ];
+
+        return view('Conta/autenticar', $data);
+
+
+    }
+
+    public function processaAutenticacao()
+    {
+
+     if($this->request->getMethod() === 'post'){
+
+        if($this->usuario->verificaPassword($this->request->getPost('password')))
+        {
+
+            session()->set('pode_editar_ate', time()+ 300); //5 minutos
+
+            
+        return redirect()->to(site_url('conta/editar'));
+
+        }else{
+
+            return redirect()->back()->with('atencao', 'Senha inválida');
+
+
+        }
+
+
+     }else{
+        return redirect()->back();
+     }
+
+
+    }
 }
---
+++ app/Views/Conta/editar.php
@@ -0,0 +1,127 @@
+  <!-- Extendendo o layout principal_site -->
+  <?= $this->extend('layout/principal_site'); ?>
+
+  <?= $this->section('titulo'); ?>
+
+  <?php echo $titulo; ?>
+
+  <?= $this->endSection(); ?>
+
+
+
+
+  <?= $this->section('estilos'); ?>
+
+  <link rel="stylesheet" href="<?php echo site_url("web/src/assets/css/conta.css"); ?>" />
+
+  <?= $this->endSection(); ?>
+
+
+
+  <?= $this->section('conteudo'); ?>
+
+  <div class="container section" id="menu" data-aos="fade-up" style="margin-top: 3em; min-height: 300px">
+
+
+      <?php echo $this->include("Conta/sidebar"); ?>
+
+      <div class="row" style="margin-top: 2em">
+
+          <div class="col-md-12 col-xs-12">
+
+              <h2 class="section-title "><?php echo esc($titulo); ?></h2>
+
+              <div class="col-md-6">
+
+                  <?php echo form_open('conta/atualizar'); ?>
+
+                  <div class="panel panel-info">
+                      <div class="panel-body">
+
+                          <div>
+
+                              <label>Nome completo</label>
+                              <input type="text" class="form-control" name="nome" value="<?php echo old('nome', esc($usuario->nome)); ?>">
+
+                          </div>
+
+                          <hr>
+
+                          <div>
+
+                              <label>Email</label>
+                              <input type="email" class="form-control" name="email" value="<?php echo old('email', esc($usuario->email)); ?>">
+
+                          </div>
+
+
+                          <hr>
+
+                          <div>
+
+                              <label>Telefone</label>
+                              <input type="tel" class="form-control sp_celphones" name="telefone" value="<?php echo old('telefone', esc($usuario->telefone)); ?>">
+
+                          </div>
+
+                          <hr>
+
+                          <div>
+
+                              <label>CPF <i class="fa fa-lock text-warner"></i></label>
+                                <div class="wel wel-sm"><?php echo old('cpf', esc($usuario->cpf)); ?></div>
+                          </div>
+
+                      </div>
+                      <div class="panel-footer">
+
+                      <button type="submit" class="btn btn-primary">Atualizar</button>
+
+                          <a href="<?php echo site_url('conta/show'); ?>" class="btn btn-danger">Cancelar</a>
+
+                      </div>
+                  </div>
+              </div>
+
+              <?php echo form_close(); ?>
+
+
+          </div>
+      </div>
+      <!-- end product -->
+  </div>
+
+
+
+
+  <?= $this->endSection(); ?>
+
+
+
+
+
+
+  <?= $this->section('scripts'); ?>
+
+
+  <script src="<?= site_url('admin/vendors/mask/jquery.mask.min.js'); ?>"></script>
+  <script src="<?= site_url('admin/vendors/mask/app.js'); ?>"></script>
+
+
+
+
+  <script>
+      /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+      function openNav() {
+          document.getElementById("mySidebar").style.width = "250px";
+          document.getElementById("main").style.marginLeft = "250px";
+      }
+
+      /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+      function closeNav() {
+          document.getElementById("mySidebar").style.width = "0";
+          document.getElementById("main").style.marginLeft = "0";
+      }
+  </script>
+
+  <?= $this->endSection(); ?>
